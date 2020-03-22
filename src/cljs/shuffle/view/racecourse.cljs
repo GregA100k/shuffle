acc7c6e207a09a8a86606c92f0f04e7b61832b0b
Greg Allen <grega100k@gmail.com>
2020-03-22 10:44:47 -0500

    add course map
    added the course map to the racecourse page and cleaned up the css.
    Also reworded the registration link and cleaned up the css on the
    welcome page.

--- src/cljs/shuffle/view/racecourse.cljs
+++ src/cljs/shuffle/view/racecourse.cljs
@@ -3,16 +3,20 @@
 (defn draw [the-tab]
   [:div.content
    [:h2 "Race Course"]
-   [:div.divflow
-     [:div 
+   [:div
+    [:div.divflow
+     [:div.onethird
+      [:div
       "The races will be run on wide grass trails.  The fully lit trails include rolling hills with excellent views of the lakes and a brilliant display of fall color.  Each loop of the course is 3.35 miles so 5K is one loop and the 10K is two loops"
-    
        [:p]
        "All three races are run on grassy ski trails over rolling hills.  The lights will be on for the 5K and 10K Night Owl races, but runners are encouraged to bring suplemental lighting with either a headlamp or hand held flashlight."
+       ]
      ]
-     [:div.twothirds.divright
+
+     [:div.divright.twothirds
        [:img {:src "img/Course_medium.png"
               :alt "Night Owl Races course map"}]
      ]
+    ]
    ]
  ])
--- src/cljs/shuffle/view/welcome.cljs
+++ src/cljs/shuffle/view/welcome.cljs
@@ -6,14 +6,29 @@
    [:div "the Night Owl Races are an excellent next step in trail running.  Step into the world of night time trail running with the Night Owl 5K and 10K races or step up to ultra distance trail running at the Six Hour Shuffle."
    ]
    [:div.divflow
-     [:div 
+     [:div.onethird
       [:h4 "Saturday October 3"]
       [:h4 "Lake Elmo Park Reserve"]
       [:h4 ""]
       [:h4 "Noon The Six Hour Shuffle"]
       [:h4 "8:00PM Night Owl 5K and 10K"]
       [:p]
+      [:div
+        [:h2 "Register at"
+          [:div.divtop
+            [:a {:href "http://ultrasignup.com/register.aspx?did=75822"
+                 :target "_blank"}
+              [:img {:src "//ultrasignup.com/images/signup.jpg" 
+                     :alt "Ultra Signup registration link"}]
+          ]]
+        ]
+      ]
       [:p]
+      [:hr]
+      [:div
+        [:img {:src "img/2020_USATF_Sanctioned_Event_Logo_350.png"
+               :alt "Sanctioned by USATF logo"
+               }] ]
       [:p]
      ]
      [:div.twothirds.divright
@@ -23,20 +38,4 @@
   ]
    [:hr]
 
-   [:div.divflow
-    [:div.divgrid.twothirds
-      [:h2 "Registration will open March 15, 2020"]
-      [:div.divtop
-        [:a {:href "http://ultrasignup.com/register.aspx?did=75822"
-           :target "_blank"}
-          [:img {:src "//ultrasignup.com/images/signup.jpg" 
-               :alt "Ultra Signup registration link"}]
-        ]
-      ]
-      ]
-     [:div.divright
-        [:img {:src "img/2020_USATF_Sanctioned_Event_Logo_350.png"
-               :alt "Sanctioned by USATF logo"
-               }] ]
-   ]
  ])

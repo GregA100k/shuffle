4ed5819c23305a711f71534a016dbe033f6bbcf8
Greg Allen <grega100k@gmail.com>
2020-07-12 17:15:51 -0500

    add covid-19 update to welcome page

--- src/cljs/shuffle/view/welcome.cljs
+++ src/cljs/shuffle/view/welcome.cljs
@@ -5,6 +5,15 @@
    [:h2 "The 7th Annual Doug Allen Memorial Trail Races will be October 3, 2020"]
    [:div "the Night Owl Races are an excellent next step in trail running.  Step into the world of night time trail running with the Night Owl 5K and 10K races or step up to ultra distance trail running at the Six Hour Shuffle."
    ]
+   [:h3 "Covid-19 Update"]
+   [:div.content.notice
+    [:p "As it stands right now, July 10, we are unable to hold the Night Owl Races.  The first point of the "
+      [:a {:href "https://staysafe.mn.gov/assets/outdoor-guidelines_tcm1152-435380.pdf"} 
+          "Stay Safe MN Outdoor Recreation Guidelines for Events"]
+      " says \"Postpone or cancel outdoor gatherings... of more than 25 people.\" While Minnesota is doing better in controlling the spread of Covid-19 than a lot of states, there is still a steady stream of new cases so it is unlikely that those rules will change."
+    ]
+    [:p "Since the goal of the Night Owl Races is to raise money for the the Doug Allen Memorial Scholarship, we are exploring options to still have an event that will be safe and hope to make the final decision on how to proceed by the end of July."
+     ]]
    [:div.divflow
      [:div.onethird
       [:h4 "Saturday October 3"]
